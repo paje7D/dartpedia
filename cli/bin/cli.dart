3aa7b1326dfba7a967f8cdf3280e64ac759c36d8
Eduardo da Silva Oliveira <eduardolegalsans@gmail.com>
2026-05-12 16:33:57 -0300

    - v7.0 - Integre o search comando em main.

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,24 +1,12 @@
-const version = '0.0.6'; // <-- Adicionado esta linha na versao '0.0.2'
+const version = '0.0.7';
 
 void main(List<String> arguments) {
-if (arguments.isEmpty || arguments.first == 'help') {
-printUsage();
-} else if (arguments.first == 'version') {
-print('Dartpedia CLI version $version');
-} else if (arguments.first == 'search') {
-// <-- Adicionado este bloco de codigo na versao '0.0.6:
-final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-searchWikipedia(inputArgs);
-} else {
-printUsage();
-}
-}
-
-void searchWikipedia(List<String>? arguments) { // <-- Adicionado esta nova funcao e argumento do tipo "?" na versao '0.0.6'
-print('searchWikipedia received arguments: $arguments');
-}
-
-void printUsage() { // <-- Adicionado esta funcao na versao '0.0.3' e '0.0.4'
-print(
-"The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
-}
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    print('Search command recognized!');
+  } else {
+    printUsage();
