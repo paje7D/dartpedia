eefc8dd5a21c4a394ff5f95a59fdec38295e281b
Eduardo da Silva Oliveira <eduardolegalsans@gmail.com>
2026-05-12 16:30:59 -0300

    - v3.0 - 06/04/2026 - Tarefa - Entrada do usuario (stdin)

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,14 +1,16 @@
-const version = '0.0.2'; // Add this line
+const version = '0.0.3'; // Add this line
 
 void main(List<String> arguments) {
-if (arguments.isEmpty) {
-print('Hello, Dart!');
-} else if (arguments.first == 'version') {
-print('Dartpedia CLI version $version');
-}
-
+	if (arguments.isEmpty || arguments.first == 'help') {
+		printUsage(); // Change this from 'Hello, Dart!'
+	} else if (arguments.first == 'version') {
+		print('Dartpedia CLI version $version');
+	} else {
+	printUsage(); // Catch-all for any unrecognized command.
 }
 
 void printUsage() { // Add this new function
-  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+	print(
+	"The following commends are valid: 'help', 'version', 'search >ARTICLE-TITLE>'");
 }
+
