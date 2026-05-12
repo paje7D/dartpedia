401d8b19742886a44fe2b817e2ad2fb51b37f753
guilherme <guilhermesantoosdpaula@gmail.com>
2026-05-12 16:33:11 -0300

    v6.0 - Versão final tarefa 02

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,24 +1,24 @@
-const version = '0.0.5'; // Add this line
+const version = '0.0.6'; // <-- Adicionado esta linha na versao '0.0.2'
 
 void main(List<String> arguments) {
 if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
-    // Add this new block:
+printUsage();
+} else if (arguments.first == 'version') {
+print('Dartpedia CLI version $version');
+} else if (arguments.first == 'search') {
+// <-- Adicionado este bloco de codigo na versao '0.0.6:
 final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
+searchWikipedia(inputArgs);
 } else {
-    printUsage();
-  }
+printUsage();
+}
 }
 
-void searchWikipedia(List<String>? arguments) { // Add this new function and add ? to arguments type
-  print('searchWikipedia received arguments: $arguments');
+void searchWikipedia(List<String>? arguments) { // <-- Adicionado esta nova funcao e argumento do tipo "?" na versao '0.0.6'
+print('searchWikipedia received arguments: $arguments');
 }
 
-void printUsage() { // Add this new function
-  print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+void printUsage() { // <-- Adicionado esta funcao na versao '0.0.3' e '0.0.4'
+print(
+"The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
 }
