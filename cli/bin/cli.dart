bc72d3d335461c5802370b6300dcb00bf5e46787
guilherme <guilhermesantoosdpaula@gmail.com>
2026-05-12 16:30:05 -0300

    v2.1 - 06/04/2026 - Tarefa - Comando search (estrutura inicial)

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,9 +1,14 @@
-const version = '0.0.1';
+const version = '0.0.2'; // Add this line
 
 void main(List<String> arguments) {
 if (arguments.isEmpty) {
-    print('Hello, Dart!');
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  }
+print('Hello, Dart!');
+} else if (arguments.first == 'version') {
+print('Dartpedia CLI version $version');
+}
+
+}
+
+void printUsage() { // Add this new function
+  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
 }
