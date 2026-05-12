0280872cef66f946c2ef62bdcc014eebefc2187f
Gustavo <gustavorodriguessouzza@gmail.com>
2026-05-12 16:32:29 -0300

    - v5.0 - 07/04/2026 - Defina a search Wikipedia funcao
    Updated version to 0.0.5 and added search functionality.

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,20 +1,24 @@
-const version = '0.0.4'; // Add this line
+const version = '0.0.5'; // Add this line
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
+if (arguments.isEmpty || arguments.first == 'help') {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
     // Add this new block:
-    print('Search command recognized!');
-  } else {
+final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+} else {
     printUsage();
   }
 }
 
+void searchWikipedia(List<String>? arguments) { // Add this new function and add ? to arguments type
+  print('searchWikipedia received arguments: $arguments');
+}
+
 void printUsage() { // Add this new function
   print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
-  );
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
 }
